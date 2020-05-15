7933d58047714a95faaed65a1b72767bdf0a5a49
brutalzinn <31475696+brutalzinn@users.noreply.github.com>
2020-05-15 10:05:37 -0300

    Revert "hgjg"
    This reverts commit 46c0fda2183f9937de52b46eb8365d784c195a48, reversing
    changes made to a653bda4fa46d22a934bf824029c53c54a57bb3b.

--- ButtonDeck/Engine/Wrappers/PluginWrapper.cs
+++ ButtonDeck/Engine/Wrappers/PluginWrapper.cs
@@ -15,14 +15,10 @@ namespace ButtonDeck.Engine.Wrappers
 
 
 <<<<<<< HEAD
-<<<<<<< HEAD
         }
         public static Forms.ActionHelperForms.ActionPlugin MainWindow { get; private set; }
         public void setFormControl(string name, string value, int posX, int posY, string type)
         {
-=======
-        public static Dictionary<string, string> users = new Dictionary<string, string>();
->>>>>>> ac0a79332a789253b59848561cc0d72763a4c710
 =======
         public static Dictionary<string, string> users = new Dictionary<string, string>();
 >>>>>>> ac0a79332a789253b59848561cc0d72763a4c710
@@ -31,7 +27,6 @@ namespace ButtonDeck.Engine.Wrappers
         {
             if (users.ContainsKey(key))
             {
-<<<<<<< HEAD
 <<<<<<< HEAD
                 case "textbox":
                    // Forms.ActionHelperForms.ActionPlugin teste = new Forms.ActionHelperForms.ActionPlugin();
@@ -51,16 +46,11 @@ namespace ButtonDeck.Engine.Wrappers
 
 
 =======
-=======
->>>>>>> ac0a79332a789253b59848561cc0d72763a4c710
                 users[key] = value;
             }
             else
             {
                 users.Add(key, value);
-<<<<<<< HEAD
->>>>>>> ac0a79332a789253b59848561cc0d72763a4c710
-=======
 >>>>>>> ac0a79332a789253b59848561cc0d72763a4c710
             }
         }
