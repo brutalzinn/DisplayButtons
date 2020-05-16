61da5ec0e180d7811768124427d1c7e714fd3439
brutalzinn <31475696+brutalzinn@users.noreply.github.com>
2020-05-16 12:12:19 -0300

    função nova

---
+++ ButtonDeck/Forms/ImageListForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ButtonDeck.Forms
+{
+    partial class ImageListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.modernButton1 = new NickAc.ModernUIDoneRight.Controls.ModernButton();
+            this.modernButton2 = new NickAc.ModernUIDoneRight.Controls.ModernButton();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // modernButton1
+            // 
+            this.modernButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.modernButton1.CustomColorScheme = false;
+            this.modernButton1.Location = new System.Drawing.Point(616, 265);
+            this.modernButton1.Name = "modernButton1";
+            this.modernButton1.Size = new System.Drawing.Size(111, 45);
+            this.modernButton1.TabIndex = 4;
+            this.modernButton1.Text = "Adicionar imagem";
+            this.modernButton1.UseVisualStyleBackColor = true;
+            // 
+            // modernButton2
+            // 
+            this.modernButton2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.modernButton2.CustomColorScheme = false;
+            this.modernButton2.Location = new System.Drawing.Point(487, 265);
+            this.modernButton2.Name = "modernButton2";
+            this.modernButton2.Size = new System.Drawing.Size(111, 45);
+            this.modernButton2.TabIndex = 5;
+            this.modernButton2.Text = "Remover imagem";
+            this.modernButton2.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(-5, -1);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(744, 253);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // ImageListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 322);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.modernButton2);
+            this.Controls.Add(this.modernButton1);
+            this.Name = "ImageListForm";
+            this.Text = "ImageListForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private NickAc.ModernUIDoneRight.Controls.ModernButton modernButton1;
+        private NickAc.ModernUIDoneRight.Controls.ModernButton modernButton2;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
