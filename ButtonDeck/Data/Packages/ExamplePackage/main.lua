49210c9bb781315f9d45089e31de9660d2f41674
brutalzinn <gordododelphi@gmail.com>
2020-05-16 17:52:06 -0300

    mudança no corpo do main.lua

--- ButtonDeck/Data/Packages/ExamplePackage/main.lua
+++ ButtonDeck/Data/Packages/ExamplePackage/main.lua
@@ -1,1 +1,26 @@
-print("I run fine!")
+function menu_ok()
+
+
+
+
+print("button ok pressionado.")
+end
+function menu_cancel()
+print("button cancel menu pressionado")
+
+end
+
+function menu()
+print("button menu pressionado.")
+
+end
+
+function ButtonUP()
+print("button up pressionado.")
+
+end
+
+function ButtonDown()
+print("button down pressionado.")
+end
+
