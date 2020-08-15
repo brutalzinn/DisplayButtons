237be0a9f22f2fe70b92332ff592c28a33266eb7
brutalzinn <gordododelphi@gmail.com>
2020-08-15 07:17:46 -0300

    adicionando

--- DisplayButtons/Properties/AssemblyInfo.cs
+++ DisplayButtons/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DisplayButtons")]
 [assembly: AssemblyProduct("DisplayButtons")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
