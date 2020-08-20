bbc29cb3acceaa5ff88acc0af665894a387e5986
brutalzinn <gordododelphi@gmail.com>
2020-08-20 16:52:11 -0300

    att

--- DisplayButtons/Bibliotecas/DeckEvents/FactoryEvents.cs
+++ DisplayButtons/Bibliotecas/DeckEvents/FactoryEvents.cs
@@ -1,4 +1,5 @@
-﻿using DisplayButtons.Backend.Utils;
+﻿using CookieProjects.ProcessWatcher;
+using DisplayButtons.Backend.Utils;
 using EventHook;
 using MoreLinq.Extensions;
 using System;
@@ -43,48 +44,20 @@ Debug.WriteLine(item.GetActionName());
                 
                 }
 
+            TimedProcessWatcher teste = new TimedProcessWatcher(1.0);
 
-
-
-
-            using (var eventHookFactory = new EventHookFactory())
+            teste.Start();
+            teste.ProcessStarted += (s, e) =>
             {
-                var keyboardWatcher = eventHookFactory.GetKeyboardWatcher();
-             //   keyboardWatcher.Start();
-                keyboardWatcher.OnKeyInput += (s, e) =>
-                {
-                    Console.WriteLine(string.Format("Key {0} event of key {1}", e.KeyData.EventType, e.KeyData.Keyname));
-                };
 
-                var mouseWatcher = eventHookFactory.GetMouseWatcher();
-            //    mouseWatcher.Start();
-                mouseWatcher.OnMouseInput += (s, e) =>
-                {
-                    Console.WriteLine(string.Format("Mouse event {0} at point {1},{2}", e.Message.ToString(), e.Point.x, e.Point.y));
-                };
+                Debug.WriteLine(string.Format("Key {0} event of key {1}", e.ProcessName,e.ProcessID));
 
-                var clipboardWatcher = eventHookFactory.GetClipboardWatcher();
-              //  clipboardWatcher.Start();
-                clipboardWatcher.OnClipboardModified += (s, e) =>
-                {
-                    Console.WriteLine(string.Format("Clipboard updated with data '{0}' of format {1}", e.Data, e.DataFormat.ToString()));
-                }; 
+            };
 
 
-                var applicationWatcher = eventHookFactory.GetApplicationWatcher();
-                applicationWatcher.Start();
-                applicationWatcher.OnApplicationWindowChange += (s, e) =>
-                {
-                    MessageBox.Show(string.Format("Application window of '{0}' with the title '{1}' was {2}", e.ApplicationData.AppName, e.ApplicationData.AppTitle, e.Event));
-                };
 
-                var printWatcher = eventHookFactory.GetPrintWatcher();
-           //     printWatcher.Start();
-                printWatcher.OnPrintEvent += (s, e) =>
-                {
-                    Console.WriteLine(string.Format("Printer '{0}' currently printing {1} pages.", e.EventData.PrinterName, e.EventData.Pages));
-                };
 
+                
                 //waiting here to keep this thread running           
                 //Console.Read();
 
@@ -94,7 +67,7 @@ Debug.WriteLine(item.GetActionName());
                 //clipboardWatcher.Stop();
                 //applicationWatcher.Stop();
                 //printWatcher.Stop();
-            }
+            
 
 
         }
