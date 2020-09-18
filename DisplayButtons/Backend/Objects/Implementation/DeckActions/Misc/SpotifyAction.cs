bdd3a0375ea9166fa4ae3dceb2144952d0f65de4
DESKTOP-4DV71MT\rober <gordododelphi@gmail.com>
2020-09-18 16:07:09 -0300

    finally we have token from spotify integration

---
+++ DisplayButtons/Backend/Objects/Implementation/DeckActions/Misc/SpotifyAction.cs
@@ -0,0 +1,142 @@
+﻿using DisplayButtons.Properties;
+using DisplayButtons.Backend.Utils.Native;
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.Linq;
+using SpotifyAPI.Web;
+using DisplayButtons.Bibliotecas.SpotifyWrapper;
+using System.Threading.Tasks;
+
+namespace DisplayButtons.Backend.Objects.Implementation.DeckActions.General
+{
+    public class SpotifyAction : AbstractDeckAction
+    {
+        public enum SpotifyMediaKeys
+        {
+            [Description("Previous")]
+            Back,
+            [Description("Next")]
+            Next,
+            [Description("Play/Pause")]
+            PlayPause,
+            [Description("Stop")]
+            Stop,
+            [Description("Volume Off")]
+            VolumeOff,
+            [Description("Volume Down")]
+            VolumeMinus,
+            [Description("Volume Up")]
+            VolumePlus
+        }
+
+        [ActionPropertyInclude]
+        [ActionPropertyDescription("Media Key")]
+        [ActionPropertyUpdateImageOnChanged]
+        public SpotifyMediaKeys Key { get; set; } = SpotifyMediaKeys.PlayPause;
+
+        public override AbstractDeckAction CloneAction()
+        {
+            return new SpotifyAction();
+        }
+
+        public override DeckActionCategory GetActionCategory()
+        {
+            return DeckActionCategory.Misc;
+        }
+
+        public override string GetActionName()
+        {
+            return Texts.rm.GetString("DECKMISCSPOTIFY", Texts.cultereinfo);
+        }
+
+        [Obsolete]
+        public override bool OnButtonClick(DeckDevice deckDevice)
+        {
+            return false;
+        }
+
+        public Keys GetKeyFromMediaKey(SpotifyMediaKeys mediaKey)
+        {
+            switch (mediaKey) {
+                case SpotifyMediaKeys.Back:
+                    return Keys.MediaPreviousTrack;
+                case SpotifyMediaKeys.Next:
+                    return Keys.MediaPreviousTrack;
+                case SpotifyMediaKeys.PlayPause:
+                    return Keys.MediaPlayPause;
+                case SpotifyMediaKeys.Stop:
+                    return Keys.MediaStop;
+                case SpotifyMediaKeys.VolumeOff:
+                    return Keys.VolumeMute;
+                case SpotifyMediaKeys.VolumeMinus:
+                    return Keys.VolumeDown;
+                case SpotifyMediaKeys.VolumePlus:
+                    return Keys.VolumeUp;
+            }
+            return Keys.None;
+        }
+
+        public override void OnButtonDown(DeckDevice deckDevice)
+        {
+
+        }
+
+        public async void CallSpotifyAuth()
+        {
+
+            
+            
+        
+           
+        }
+        public override void OnButtonUp(DeckDevice deckDevice)
+        {
+            switch (Key)
+            {
+                case SpotifyMediaKeys.PlayPause:
+
+                     Task.FromResult( Spotify.Main());
+
+                    break;
+
+
+
+
+            }
+        }
+
+        public override DeckImage GetDefaultItemImage()
+        {
+            var img = GetKey(Key);
+            if (img != null)
+                return new DeckImage(img);
+            return base.GetDefaultItemImage();
+        }
+
+        private Bitmap GetKey(SpotifyMediaKeys key)
+        {
+            switch (key) {
+                case SpotifyMediaKeys.Back:
+                    return Resources.img_item_media_back;
+                case SpotifyMediaKeys.Next:
+                    return Resources.img_item_media_next;
+                case SpotifyMediaKeys.PlayPause:
+                    return Resources.img_item_media_playpause;
+                case SpotifyMediaKeys.Stop:
+                    return Resources.img_item_media_stop;
+                case SpotifyMediaKeys.VolumeOff:
+                    return Resources.img_item_media_volumeoff;
+                case SpotifyMediaKeys.VolumeMinus:
+                    return Resources.img_item_media_volumedown;
+                case SpotifyMediaKeys.VolumePlus:
+                    return Resources.img_item_media_volumeup;
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ DisplayButtons/Bibliotecas/SpotifyWrapper/Spotify.cs
@@ -0,0 +1,108 @@
+﻿
+using Newtonsoft.Json;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Threading.Tasks;
+using static SpotifyAPI.Web.Scopes;
+
+
+namespace DisplayButtons.Bibliotecas.SpotifyWrapper
+{
+    public static class Spotify
+    {
+        private const string CredentialsPath = "credentials.json";
+        private static readonly string? clientId = "d1362b6ae88a4254900b41bdc1489503";
+        private static readonly EmbedIOAuthServer _server = new EmbedIOAuthServer(new Uri("http://localhost:5000/callback"), 5000);
+
+        
+        public static async Task<int> Main()
+        {
+            // This is a bug in the SWAN Logging library, need this hack to bring back the cursor
+           
+
+            if (string.IsNullOrEmpty(clientId))
+            {
+                throw new NullReferenceException(
+                  "Please set SPOTIFY_CLIENT_ID via environment variables before starting the program"
+                );
+            }
+
+            if (File.Exists(CredentialsPath))
+            {
+                await Start();
+            }
+            else
+            {
+                await StartAuthentication();
+            }
+
+            Console.ReadKey();
+            return 0;
+        }
+
+        private static async Task Start()
+        {
+            var json = await File.ReadAllTextAsync(CredentialsPath);
+            var token = JsonConvert.DeserializeObject<PKCETokenResponse>(json);
+
+            var authenticator = new PKCEAuthenticator(clientId!, token);
+            authenticator.TokenRefreshed += (sender, token) => File.WriteAllText(CredentialsPath, JsonConvert.SerializeObject(token));
+
+            var config = SpotifyClientConfig.CreateDefault()
+              .WithAuthenticator(authenticator);
+
+            var spotify = new SpotifyClient(config);
+
+            var me = await spotify.UserProfile.Current();
+            Console.WriteLine($"Welcome {me.DisplayName} ({me.Id}), you're authenticated!");
+
+            var playlists = await spotify.PaginateAll(await spotify.Playlists.CurrentUsers().ConfigureAwait(false));
+            Console.WriteLine($"Total Playlists in your Account: {playlists.Count}");
+
+            _server.Dispose();
+            Environment.Exit(0);
+        }
+
+        private static async Task StartAuthentication()
+        {
+            var (verifier, challenge) = PKCEUtil.GenerateCodes();
+
+            await _server.Start();
+            _server.AuthorizationCodeReceived += async (sender, response) =>
+            {
+                await _server.Stop();
+                PKCETokenResponse token = await new OAuthClient().RequestToken(
+                  new PKCETokenRequest(clientId!, response.Code, _server.BaseUri, verifier)
+                );
+
+                await File.WriteAllTextAsync(CredentialsPath, JsonConvert.SerializeObject(token));
+                await Start();
+            };
+
+            var request = new LoginRequest(_server.BaseUri, clientId!, LoginRequest.ResponseType.Code)
+            {
+                CodeChallenge = challenge,
+                CodeChallengeMethod = "S256",
+                Scope = new List<string> { UserReadEmail, UserReadPrivate, PlaylistReadPrivate, PlaylistReadCollaborative }
+            };
+
+            Uri uri = request.ToUri();
+            try
+            {
+                BrowserUtil.Open(uri);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to open URL, manually open: {0}", uri);
+            }
+        }
+    }
+}
