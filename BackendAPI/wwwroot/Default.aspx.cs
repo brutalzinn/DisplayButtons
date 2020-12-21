acc7d6549cd35a2b39df45ff0e60bb600af2219f
DESKTOP-4DV71MT\rober <gordododelphi@gmail.com>
2020-12-21 17:20:49 -0300

    sending changes

--- BackendAPI/wwwroot/Default.aspx.cs
+++ BackendAPI/wwwroot/Default.aspx.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
-public partial class _Default : Page
+using System.Windows.Controls;
+
+public partial class Default : Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
