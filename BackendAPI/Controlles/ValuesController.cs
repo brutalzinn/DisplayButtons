b9a7038ab0ebb2bc5f725ebbd405e26e8a3128ee
DESKTOP-4DV71MT\rober <gordododelphi@gmail.com>
2020-12-21 08:24:28 -0300

    asp migration

--- BackendAPI/Controlles/ValuesController.cs
+++ BackendAPI/Controlles/ValuesController.cs
@@ -1,24 +1,42 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using BackendAPI.Objects;
+using BackendAPI.Sdk;
+using BackendAPI.Utils;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyWinFormsApp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class ButtonClickController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<string> Get()
-        {
-            string text = "testando o abacatão!";
-          
-            return text;
-        }
+        public ButtonShapes.ButtonAction PerformedAction { get; set; }
 
         [HttpGet("{id}")]
-        public ActionResult Get(string id)
+        public ActionResult Get(int id,int action = 1)
         {
-          
+            PerformedAction = (ButtonShapes.ButtonAction)action;
+
+            Debug.WriteLine("ACTION: " + action+ "Button id " + id);
+            // DevicePersistManager.GetDeckDeviceFromConnectionGuid(DevicePersistManager.GuidsFromConnections)
+            DeckDevice device;
+            if (Debugger.IsAttached && DevicePersistManager.IsDeviceTest)
+            {
+                device = DevicePersistManager.DeviceTest;
+
+            }
+            else
+            {
+
+                device = DevicePersistManager.DeckDevicesFromConnection.FirstOrDefault().Value;
+
+            }
+
+            device.OnButtonInteraction(PerformedAction, id);
+
+            //  return id;
             return Ok();
         }
     }
