6291d9efc1f5a76c1d3c5c477b5993941b16c1c9
DESKTOP-4DV71MT\rober <gordododelphi@gmail.com>
2020-12-24 17:02:48 -0300

    Revert "sending changes"
    This reverts commit acc7d6549cd35a2b39df45ff0e60bb600af2219f.

--- BackendAPI/wwwroot/Default.aspx.cs
+++ BackendAPI/wwwroot/Default.aspx.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
-using System.Windows.Controls;
-
-public partial class Default : Page
+public partial class _Default : Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
